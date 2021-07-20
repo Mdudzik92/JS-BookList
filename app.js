0743fd485154a5a6645b60c4f52f86131fec9836
Matt Dudzik <mdudzik92@gmail.com>
2021-07-20 01:20:10 -0500

    Added an event listener to submit a book and create new objects

--- app.js
+++ app.js
@@ -52,11 +52,25 @@ class UI {
 	}
 }
 
-// Store Class: Handles Storage (Local Storage)
+// Store Class: Handles Storage
 
-// Event: Dispay Books
+// Event: Display Books
 document.addEventListener("DOMContentLoaded", UI.displayBooks);
 
 // Event: Add a Book
+document.querySelector("#book-form").addEventListener("submit", (e) => {
+	// Prevent actual submit
+	e.preventDefault();
+
+	// Get form values
+	const title = document.querySelector("#title").value;
+	const author = document.querySelector("#author").value;
+	const isbn = document.querySelector("#isbn").value;
+
+	// Instantiate book
+	const book = new Book(title, author, isbn);
+
+	console.log(book);
+});
 
 // Event: Remove a Book
